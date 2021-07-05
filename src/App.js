01cd7e8b5b2f4c12f1812b525cf24bbd36654689
Emilie <adamanska@gmail.com>
2021-07-05 09:19:51 +0200

    cleaning

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
+
+
 function App() {
+
+  const [pos, setPos] = React.useState([])
+  const [reporting, setReporting] = React.useState(false)
+
+  const modifyArr = (which, what) => {
+    const updatedArr = [...pos];
+    if (updatedArr[which] + what === 5) {
+      updatedArr[which] = 0
+    }
+    else if (updatedArr[which] + what === -1) {
+      updatedArr[which] = 4
+    } else {
+      updatedArr[which] = updatedArr[which]+what;
+    }
+    setPos(updatedArr);
+  }
+
+  const keysHandler = (event) => {
+    if (pos.length > 0) {
+      if (event.keyCode === 37) {
+        console.log('left ' + pos)
+        modifyArr(1, -1)
+      } else if (event.keyCode === 38) {
+        modifyArr(0, -1)
+      } else if (event.keyCode === 39) {
+        modifyArr(1, 1)
+      } else if (event.keyCode === 40) {
+        modifyArr(0, 1)
+      } 
+    }
+      
+    };
+
+  useEffect(() => {
+    
+    window.addEventListener('keydown', keysHandler);
+    return () => {
+      window.removeEventListener('keydown', keysHandler);
+    };
+  }, [keysHandler]);
+
+
+  let cellsmap = []
+  let x = 0
+  let y = 0
+  for (let i = 0; i < 25; i++){
+    cellsmap.push([x, y])
+    y++
+    if (y === 5) { y = 0; x++}
+  }
+  
+
+  const place = () => {
+    setPos(cellsmap[Math.floor(Math.random() * cellsmap.length)])
+  }
+
+  const report = () => {
+    setReporting(true)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 >Use the keyboard arrows to move</h1>
+      <button onClick={() => place()}>{pos.length > 0 ? 'Random position' : 'Add droid'}</button>
+      {pos.length > 0 &&
+        <button className='report' onClick={() => report()}>{ reporting ? `Droid is at ${pos[0]}, ${pos[1]}`:'Report'}</button>
+      }
+      
+      <div className="map">
+        {cellsmap.map((i) => {
+          console.log('Render Placing: ' + pos)
+          
+          return pos[0] === i[0] && pos[1] === i[1] ?
+            <div className="cell" key={i} id={i} data-testid="cell">
+              <img src='./droid.png' alt="droid"/>
+            </div>
+            :
+            <div className="cell" key={i} id={i} data-testid="cell">
+            </div>
+          
+        })}
+        
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,37 @@
-import { render, screen } from '@testing-library/react';
+// import { getByAltText, getByRole, queryByAltText, render,  } from '@testing-library/react'
+// import { screen } from '@testing-library/dom'
 import App from './App';
+import React from 'react';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({adapter: new Adapter() })
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Testing app.jsx => ', () => {
+
+  const wrapper = shallow(<App />);
+
+  test("h1 text displayed", () => {
+    expect(wrapper.find('h1').contains("Use the keyboard arrows to move"));
+  });
+
+  test("button displayed", () => {
+    expect(wrapper.find('button')).toBeTruthy();
+  });
+
+  test("cells displayed", () => {
+    expect(wrapper.find("cell")).toBeTruthy();
+  });
+
+  test("Img not displayed", () => {
+    expect(wrapper.contains(<img src='./droid.png' alt="droid"/>)).toBe(false)
+  })
+
+  test('Testing button click', () => {
+    const mockBtnClick = jest.fn();
+
+    const button = shallow((<button onClick={mockBtnClick}>Ok!</button>));
+    button.find('button').simulate('click');
+    expect(mockBtnClick.mock.calls.length).toEqual(1);
+  });
+  
 });
